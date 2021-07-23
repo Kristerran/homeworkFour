e14857de0fc2d8e0d81ef6971589740c5fe3edf4
kristerran <kristopherbaily@gmail.com>
2021-07-23 10:38:30 -0700

    Added function to show one question at a time

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,128 @@
+// Variables declared at startup
 const quizEl = document.getElementById('quizContent')
 const timerEl = document.getElementById('timerContent')
 const startButton = document.getElementById('start')
-
+const submitButton = document.getElementById('submitQuiz')
+const quizSource = [
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "ESLint"
+    },
+    correctAnswer: "c"
+  }
+];
 let timer = 11
+submitButton.classList.add('invisible')
+let score = 0
+// Add event listener to start startButton, this will start a function that encloses all of our functions and starts the quiz
+document.getElementById('start').addEventListener('click', function () {
+  // Declare functions
+  buildQuiz()
+  function buildQuiz() {
+    const output = []
+    submitButton.classList.remove('invisible')
+    quizSource.forEach(
+      (currentQuestion, questionNumber) => {
 
-document.getElementById('start').addEventListener('click', startTimer) 
+        // variable to store the list of possible answers
+        const answers = [];
 
-function startTimer(){
-    startButton.classList.add('invisible')
-    var set = setInterval(function () {
-        buildQuiz()
-        didWin = checkWin()
-        if (timer === 0) {
-            clearInterval(set)
-            gameOver()
+        // and for each available answer...
+        for (letter in currentQuestion.answers) {
+
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
         }
-        else if (didWin === true) {
-            winGame()
-        }
-        else if (didWin === false) {
-            clearInterval(set)
-            loseGame()
-        }
-        else{
-            timer -= 1
-            timerEl.innerHTML = (timer)
-        }
-    }, 1000)
-}
-function buildQuiz() {
-}
-function gameOver() {
+
+        // add this question and its answers to the output
+        output.push(
+          `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+        );
+      }
+    );
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizEl.innerHTML = output.join('');
+  }
+  function gameOver() {
     alert('GAME OVER')
     timer = 11
     timerEl.innerHTML = ""
     startButton.classList.remove('invisible')
-}
-function loseGame() {
+  }
+  function loseGame() {
     alert('wrong answer')
     timer -= 5
-    timerEl.innerHTML = ""
-}
-function winGame() {
+    nextSlide()
+  }
+  function winGame() {
     alert('YOU WON!')
     timer += 5
-    timerEl.innerHTML = ""
-}
-function checkWin(){
-    var didWin = false
+    score += 1
+    console.log(score)
+    nextSlide()
+  }
+  function checkWin() {
+    var didWin = ""
     return didWin
-}
+  }
+  function nextSlide() {
+
+  }
+  // additional variables to select slides (allows us to control which question is visible at a given time)
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+  slides[currentSlide].classList.add('activeSlide')
+  startTimer()
+  function startTimer() {
+    startButton.classList.add('invisible')
+    var set = setInterval(function () {
+      didWin = checkWin()
+      if (timer === 0) {
+        clearInterval(set)
+        gameOver()
+      }
+      else if (didWin === true) {
+        winGame()
+      }
+      else if (didWin === false) {
+        clearInterval(set)
+        loseGame()
+      }
+      else {
+        timer -= 1
+        timerEl.innerHTML = (timer)
+      }
+    }, 1000)
+  }
+})
